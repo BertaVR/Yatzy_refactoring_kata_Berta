cc76123a01c82508e6acd3054791e56bf57b13dd
BertaVR <bverges@cifpfbmoll.eu>
2020-12-31 01:58:30 +0100

    refactor: :art: he refactorizado todos los tipos

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -4,75 +4,82 @@ from yatzy import Yatzy
 # available from http://pytest.org/
 
 def test_chance(): #el resultado de chance es la suma de todos los dados
-        assert Yatzy.chance([2,3,4,5,1]) == 15
-        assert Yatzy.chance([3,3,4,5,1]) == 16
+        assert Yatzy.chance(1,1,1,1,1) == 5
+        assert Yatzy.chance(2,3,4,5,1) == 15
+        assert Yatzy.chance(3,3,4,5,1) == 16
 
 
 def test_yatzy(): #si todos son iguales devuelve 50, si no 0
-        assert Yatzy.yatzy([4,4,4,4,4]) == 50
-        assert Yatzy.yatzy([6,6,6,6,6]) == 50
-        assert Yatzy.yatzy([6,6,6,6,3]) == 0
+        assert Yatzy.yatzy(4,4,4,4,4) == 50
+        assert Yatzy.yatzy(6,6,6,6,6) == 50
+        assert Yatzy.yatzy(6,6,6,6,3) == 0
 
 
 def test_ones(): #suma todos los unos
-        assert Yatzy.ones([1,2,3,4,5]) == 1
-        assert Yatzy.ones([1,2,1,4,5]) == 2
-        assert Yatzy.ones([6,2,2,4,5]) == 0
-        assert Yatzy.ones([1,2,1,1,1]) == 4
+        assert Yatzy.ones(1,2,3,4,5) == 1
+        assert Yatzy.ones(1,2,1,4,5) == 2
+        assert Yatzy.ones(6,2,2,4,5) == 0
+        assert Yatzy.ones(1,2,1,1,1) == 4
 
 
 def test_twos(): #suma todos los doses (el valor, no la cuenta)
-        assert Yatzy.twos([1,2,3,2,6]) == 4
-        assert Yatzy.twos([2,2,2,2,2]) == 10
+        assert Yatzy.twos(1,2,3,2,6) == 4
+        assert Yatzy.twos(2,2,2,2,2) == 10
+        assert Yatzy.twos(1,6,3,6,6) == 0
+
 
 
 def test_threes():
-        assert Yatzy.threes([1,2,3,2,3]) == 6
-        assert Yatzy.threes([2,3,3,3,3]) == 12
+        assert Yatzy.threes(1,2,3,2,3) == 6
+        assert Yatzy.threes(2,3,3,3,3) == 12
+        assert Yatzy.threes(2,1,1,1,1) == 0
 
 
 def test_fours_test():
-        assert Yatzy([4,4,4,5,5]).fours() == 12
-        assert Yatzy([4,4,5,5,5]).fours() == 8
-        assert Yatzy([4,5,5,5,5]).fours() == 4
+        assert Yatzy(4,4,4,5,5).fours() == 12
+        assert Yatzy(4,4,5,5,5).fours() == 8
+        assert Yatzy(4,5,5,5,5).fours() == 4
+        assert Yatzy(2,1,1,1,1).fours() == 0
+
 
 
 def test_fives():
-        assert ([4,4,4,5,5]).fives() == 10
-        assert ([4,4,5,5,5]).fives() == 15
-        assert ([4,5,5,5,5]).fives() == 20
-  
+        assert Yatzy(4,4,4,5,5).fives() == 10
+        assert Yatzy(4,4,5,5,5).fives() == 15
+        assert Yatzy(4,5,5,5,5).fives() == 20
+        assert Yatzy(4,4,4,4,4).fives() == 0
+
 
 def test_sixes_test():
-        assert 0 == Yatzy(4,4,4,5,5).sixes()
-        assert 6 == Yatzy(4,4,6,5,5).sixes()
-        assert 18 == Yatzy(6,5,6,6,5).sixes()
-  
+        assert Yatzy(4,4,4,5,5).sixes() == 0
+        assert Yatzy(4,4,6,5,5).sixes() == 6
+        assert Yatzy(6,5,6,6,5).sixes() == 18
+
 
 def test_one_pair():
-        assert Yatzy.score_pair([1,4,3,5,6]) == 0
-        assert Yatzy.score_pair([3,4,3,5,6]) == 6
-        assert Yatzy.score_pair([5,3,3,3,5]) == 10
-        assert Yatzy.score_pair([5,3,6,6,5]) == 12
-        assert Yatzy.score_pair([5,3,6,6,3]) == 12
+        assert Yatzy.score_pair(1,4,3,5,6) == 0
+        assert Yatzy.score_pair(3,4,3,5,6) == 6
+        assert Yatzy.score_pair(5,3,3,3,5) == 10
+        assert Yatzy.score_pair(5,3,6,6,5) == 12
+        assert Yatzy.score_pair(5,3,6,6,3) == 12
 
 
 def test_two_Pair():
-        assert Yatzy.two_pair([3,3,5,4,5]) == 16
-        assert Yatzy.two_pair([3,3,6,6,6]) == 18
-        assert Yatzy.two_pair([3,3,6,5,4]) == 0
+        assert Yatzy.two_pair(3,3,5,4,5) == 16
+        assert Yatzy.two_pair(3,3,6,6,6) == 18
+        assert Yatzy.two_pair(3,3,6,5,4) == 0
 
 def test_three_of_a_kind():
-        assert Yatzy.three_of_a_kind([3,3,3,4,5]) == 9
-        assert Yatzy.three_of_a_kind([5,3,5,4,5]) == 15
-        assert Yatzy.three_of_a_kind([3,3,3,3,5]) == 9
-  
+        assert Yatzy.three_of_a_kind(3,3,3,4,5) == 9
+        assert Yatzy.three_of_a_kind(5,3,5,4,5) == 15
+        assert Yatzy.three_of_a_kind(3,3,3,3,5) == 9
+
 
 def test_four_of_a_knd():
-        assert Yatzy.four_of_a_kind([3,3,3,3,5]) == 12
-        assert Yatzy.four_of_a_kind([5,5,5,4,5]) == 20
-        assert Yatzy.four_of_a_kind([3,3,3,3,3]) == 12
-        assert Yatzy.four_of_a_kind([3,3,3,2,1]) == 0
+        assert Yatzy.four_of_a_kind(3,3,3,3,5) == 12
+        assert Yatzy.four_of_a_kind(5,5,5,4,5) == 20
+        assert Yatzy.four_of_a_kind(3,3,3,3,3) == 12
+        assert Yatzy.four_of_a_kind(3,3,3,2,1) == 0
 
 
 
@@ -80,25 +87,30 @@ def test_four_of_a_knd():
 
 
 def test_smallStraight():
-        assert Yatzy.smallStraight([1,2,3,4,5]) == 15
-        assert Yatzy.smallStraight([2,3,4,5,1]) == 15
-        assert Yatzy.smallStraight([1,2,2,4,5]) == 0
-        assert Yatzy.smallStraight([2,3,4,5,6]) == 0
-        assert Yatzy.smallStraight([4,1,5,2,3]) == 15
+        assert Yatzy.smallStraight(1,2,3,4,5) == 15
+        assert Yatzy.smallStraight(2,3,4,5,1) == 15
+        assert Yatzy.smallStraight(1,2,2,4,5) == 0
+        assert Yatzy.smallStraight(2,3,4,5,6) == 0
+        assert Yatzy.smallStraight(4,1,5,2,3) == 15
 
 
-  
+
 
 def test_largeStraight():
-        assert Yatzy.largeStraight([6,2,4,5,3]) == 20
-        assert Yatzy.largeStraight([6,2,3,4,5]) == 20
-        assert Yatzy.largeStraight([2,3,4,5,6]) == 20
-        assert Yatzy.largeStraight([1,2,2,4,5]) == 0
-        assert Yatzy.smallStraight([4,1,5,2,3]) == 0
+        assert Yatzy.largeStraight(6,2,4,5,3) == 20
+        assert Yatzy.largeStraight(6,2,3,4,5) == 20
+        assert Yatzy.largeStraight(2,3,4,5,6) == 20
+        assert Yatzy.largeStraight(1,2,2,4,5) == 0
+        assert Yatzy.largeStraight(4,1,5,2,3) == 0
 
-  
+
 
 def test_fullHouse():
-        assert 18 == Yatzy.fullHouse(6,2,2,2,6)
-        assert 0 == Yatzy.fullHouse(2,3,4,5,6)
-   
+        assert Yatzy.fullHouse(6,2,2,2,6) == 18
+        assert Yatzy.fullHouse(2,3,4,5,6) == 0
+        assert Yatzy.fullHouse(6,2,1,2,6) == 0
+        assert Yatzy.fullHouse(6,2,6,1,6) == 0
+        assert Yatzy.fullHouse(6,6,2,2,2) == 18
+        assert Yatzy.fullHouse(6,6,6,2,2) == 22
+        assert Yatzy.fullHouse(1,2,3,1,6) == 0
+
--- python/yatzy.py
+++ python/yatzy.py
@@ -1,125 +1,126 @@
 class Yatzy:
 
-    @staticmethod
-    def chance(dice_lst):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
-
-        total = sum(dice_lst)
-        return total 
-#coge como parámetro una lista de los parámetros de los dados y el resultado es el sumatorio de los valores de la lista (antes cogía los valores uno por uno)
+    def __init__(self, *dice):
+        self.dice = list(dice)
 
 
     @staticmethod
-    def yatzy(dice_lst):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
+    def chance(*dice):
+        assert len(dice )== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
+
+        total_points = sum(dice)
+        return total_points
+#coge como parámetro una lista de los dados y el resultado es el sumatorio de los valores de la lista 
 
 
-        check_if_all_same = dice_lst.count(dice_lst[0]) == len(dice_lst) 
-# esto chequea que todos los elementos de la lista sean como el primero, !!!!!!plantéate si se ve más claro poniéndolo en dos variables separadas (una que sea el count y otra que sea la que chequee si es igual a la longitud de la lista)
-        if check_if_all_same == True:
+    @staticmethod
+    def yatzy(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
+
+        if dice.count(dice[0]) == len(dice):
+# esto chequea que todos los elementos de la lista sean como el primero, tmb puedo cambiarlo por ==5 y quizá se lee mejor
             points_total = 50
         else: 
             points_total = 0
         return points_total
 
-    def __init__(self, dice_lst, number):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
-
-        self.count = dice_lst.count(number)
-        self.points_total = self.count * number
-
     @staticmethod
-    def ones(dice_lst):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
+    def ones(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
 
 
-        points_total = dice_lst.count(1)
+        points_total = dice.count(1)
         return points_total
     
 
     @staticmethod
-    def twos(dice_lst):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
+    def twos(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
 
-        points_total = dice_lst.count(2) * 2
+        points_total = dice.count(2) * 2
         return points_total
     
     
     @staticmethod
-    def threes(dice_lst):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
+    def threes(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
 
         
-        points_total = dice_lst.count(3) * 3
+        points_total = dice.count(3) * 3
         return points_total
     
     
 
 
     
-    def fours(self):
-        sum = 0
-        for at in range(5):
-            if (self.dice[at] == 4): 
-                sum += 4
-        return sum
+    def fours(self, *dice):
+        assert len(self.dice)== 5
+        assert max(self.dice) <= 6
+        assert min(self.dice) >= 1
+
+        points_total = self.dice.count(4) * 4
+        return points_total
+
     
 
-    def fives(self):
-        s = 0
-        i = 0
-        for i in range(len(self.dice)): 
-            if (self.dice[i] == 5):
-                s = s + 5
-        return s
+    def fives(self, *dice):
+        assert len(self.dice)== 5
+        assert max(self.dice) <= 6
+        assert min(self.dice) >= 1
+
+        points_total = self.dice.count(5) * 5
+
+        return points_total
     
 
-    def sixes(self):
-        sum = 0
-        for at in range(len(self.dice)): 
-            if (self.dice[at] == 6):
-                sum = sum + 6
-        return sum
-    
+    def sixes(self, *dice):
+        assert len(self.dice)== 5
+        assert max(self.dice) <= 6
+        assert min(self.dice) >= 1
+
+        points_total = self.dice.count(6) * 6
+
+        return points_total
+
+
+
     @staticmethod
-    def score_pair(dice_lst):
-        assert len(dice_lst)== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
+    def score_pair(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
 
         repeated_lst = [0] 
 #El acumulador parte con un 0 en lugar de estar vacío porque de esta manera si no hay parejas la puntuación será 0
-        for dice in dice_lst:
-            if dice_lst.count(dice) >= 2:
-                repeated_lst.append(dice)
+        for number in dice:
+            if dice.count(number) >= 2:
+                repeated_lst.append(number)
 
         points_total = max(repeated_lst) * 2
         return points_total
                 
     
     @staticmethod
-    def two_pair(dice_lst):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
+    def two_pair(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
 
         repeated_lst = [] 
 #aquí no pongo 0 porque si no me cargo la puntuación, si no el 0 sería el min 
-        for dice in dice_lst:
-            if dice_lst.count(dice) >= 2:
-                repeated_lst.append(dice)
+        for number in dice:
+            if dice.count(number) >= 2:
+                repeated_lst.append(number)
 
         if len(repeated_lst) >= 4:
             points_total = min(repeated_lst) * 2 + max(repeated_lst) * 2 
@@ -130,31 +131,31 @@ class Yatzy:
         return points_total
 
     @staticmethod
-    def three_of_a_kind(dice_lst):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
+    def three_of_a_kind(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
 
         points_total = 0 
 #en vez de la lista, aquí ya me aseguro que si no hay trío la puntuación no cambiará y se quedará en 0
-        for dice in dice_lst:
-            if dice_lst.count(dice) >= 3:
-                points_total = dice * 3
+        for number in dice:
+            if dice.count(number) >= 3:
+                points_total = number * 3
                 break #break para que no siga rodando repitiendo números
 #aquí no haría falta hacer un acumulador y un if independiente poque solo puede haner un trío
 
         return points_total
         
     @staticmethod
-    def four_of_a_kind(dice_lst):
-        assert len(dice_lst )== 5
-        assert max(dice_lst) <= 6
-        assert min(dice_lst) >= 1
+    def four_of_a_kind(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
 
         points_total = 0 
-        for dice in dice_lst:
-            if dice_lst.count(dice) >= 4:
-                points_total = dice * 4 
+        for number in dice:
+            if dice.count(number) >= 4:
+                points_total = number * 4 
                 break 
 #esto es calcado al de los tríos
 
@@ -164,8 +165,12 @@ class Yatzy:
     
 
     @staticmethod
-    def smallStraight(dice_lst):
-        is_small_straight = sorted(dice_lst) == [1,2,3,4,5]
+    def smallStraight(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
+
+        is_small_straight = sorted(dice) == [1,2,3,4,5]
         if is_small_straight == True:
             points_total = 15  #sumatorio de los números de la lista (dados)
         else:
@@ -176,45 +181,43 @@ class Yatzy:
     
 
     @staticmethod
-    def largeStraight(dice_lst):
-        is_large_straight = sorted(dice_lst) == [2,3,4,5,6]
+    def largeStraight(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
+
+        is_large_straight = sorted(dice) == [2,3,4,5,6] 
         if is_large_straight == True:
             points_total = 20 #sumatorio de los números de la lista (dados)
         else:
             points_total = 0
 
         return points_total
+
     
 
     @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
+    def fullHouse(*dice):
+        assert len(dice)== 5
+        assert max(dice) <= 6
+        assert min(dice) >= 1
 
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
+        pair = False
+        three = False
 
-        for i in range(6):
-            if (tallies[i] == 2): 
-                _2 = True
-                _2_at = i+1
-            
+        for number in dice:
+            if dice.count(number) == 2:
+                pair = True
+            elif dice.count(number) == 3:
+                three = True
 
-        for i in range(6):
-            if (tallies[i] == 3): 
-                _3 = True
-                _3_at = i+1
-            
 
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
+            elif dice.count(number) != 2 and dice.count(number) != 3:
+                points_total = 0
+                break
+            if three == True and pair== True:
+                points_total = sum(dice)
+                break
+        return points_total
+#vale esto es un bucle que mira de cada dado si hay 2 o 3 de ese mismo número. En cuanto se encuentre un pair y un three el bucle rompe y la puntuación es el sumatorio de los numbers, si encuentra un número de dados repetidos que no sea 2 o 3 el bucle rompe y la puntuación es 0.  
+
